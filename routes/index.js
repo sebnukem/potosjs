d6183182fc35dda0f5e29cd0b9d9d08f58517fc1
Sebastien Nicoud <snicoud@expedia.com>
2016-12-08 22:17:05 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var potos = require('../x/potos');
+var conf = require('../conf');
+
+var pixpath = '';
+router.init = function (data) {
+	pixpath = data;
+};
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', { title: 'Express' });
+});
+
+// ls with callback
+router.get('/pixcb', function(req, res, next) {
+	potos.pixcb(__dirname + "/../public/" + conf.pixpath + "/", req.query.path, function (data) {
+		data.querypath = req.query.path;
+		data.path = conf.pixpath + "/" + req.query.path;
+		if (req.query.fmt === 'json')
+			res.json(data);
+		res.render('pix', data);
+	});
+});
+
+// ls with promise
+router.get('/pixp', function(req, res, next) {
+	potos.pixp(__dirname + "/../public/" + conf.pixpath + "/", req.query.path)
+	.then(function (data) {
+		data.querypath = req.query.path;
+		data.path = conf.pixpath + "/" + req.query.path;
+		if (req.query.fmt === 'json')
+			res.json(data);
+		res.render('pix', data);
+	})
+//	.then(res.json)
+//	.catch(function (err) { return "err"; });
+	.catch(next);
+});
+
+
+module.exports = router;
---
+++ x/utils.js
@@ -0,0 +1,59 @@
+var _ = require('lodash');
+
+exports.prettyJson = prettyJson;
+exports.detectMime = detectMime;
+exports.isImage = isImage;
+exports.filterImages = filterImages;
+
+function prettyJson(str, indent) {
+	if (!indent) return str;
+	var i = +indent;
+	if (!(i > 0 && i < 9)) i = 2;
+	return JSON.stringify(JSON.parse(str), null, i);
+}
+
+var mmmagic = require('mmmagic');
+var magic = new mmmagic.Magic(mmmagic.MAGIC_MIME_TYPE);
+
+function detectMime(filename) {
+	console.log("u:detectMime "+filename);
+	return new Promise(function (resolve, reject) {
+		magic.detectFile(filename, function(err, mime) {
+			if (err) {
+				console.log(err);
+				reject(err);
+			}
+			console.log("detected "+filename+" mime type "+mime);
+			if (!mime) reject('undefineds');
+			resolve(mime);
+		});
+	});
+};
+
+function isImage(filename) {
+	console.log("u:isImage "+filename);
+	return detectMime(filename)
+	.then(function (mime) {
+		return _.startsWith(mime, 'image');
+	});
+};
+
+function filterImages(fspath, filenames) {
+	var images = [],
+		pending = filenames.length
+	console.log("u:filterImages "+fspath+","+filenames);
+
+	return new Promise(function (resolve, reject) {
+		filenames.forEach(function(filename, index) {
+			isImage(fspath + "/" + filename)
+			.then(function (isit) {
+				if (isit) {
+					console.log(filename+" is an image");
+					images.push(filename);
+				}
+				pending--;
+				if (pending === 0) resolve(images);
+			});
+		});
+	});
+};
