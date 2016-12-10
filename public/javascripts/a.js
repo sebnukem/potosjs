28d711299f9d53b53a15086b2278fae4816b4945
Sebastien Nicoud <snicoud@expedia.com>
2016-12-10 12:16:44 -0500

    thumb zoom in/out

--- public/javascripts/a.js
+++ public/javascripts/a.js
@@ -1,7 +1,35 @@
+
 $(function() {
+
+  var zoom = 50;
 
   // collapse all spaces between elements
   $('.collapse').each(function () {
     $(this).html($(this).html().replace(/>\s+</g, "><"));
   });
+
+  // thumb resize
+  function doZoom(zoom) {
+    zoom = Math.round(zoom);
+    if (zoom < 10) zoom = 10;
+    if (zoom > 1000) zoom = 1000;
+    console.log('zooming to '+zoom);
+    $('.pic').each(function () {
+      $(this).css('maxWidth', zoom);
+    });
+  }
+
+  $('body').keydown(function (e) {
+    if (e.keyCode === 173) { // -_ key
+      zoom *= 0.9;
+      doZoom(zoom);
+      return false;
+    }
+    if (e.keyCode === 61) { // =+ key
+      zoom *= 1.1;
+      doZoom(zoom);
+      return false;
+    }
+  });
+
 });
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var potos = require('../x/potos');
-var conf = require('../conf');
 var path = require('path');
+var conf = require('../conf');
+var potos = require('../x/potos');
 var u = require('../x/utils');
 
 // ls with promise
 router.get('/pix', function(req, res, next) {
 	var query_path = path.normalize(req.query.path);
 	var fsdir = path.normalize(__dirname + "/../public/" + conf.pixpath + "/");
+
 	potos.pix(fsdir, query_path)
 	.then(function (data) {
 		data.querypath = query_path;
 		data.path = path.normalize("/" + conf.pixpath + "/" + query_path);
 		data.breadcrumbs = u.splitPath(query_path);
 		console.log("/pix data", data);
+
 		if (req.query.fmt === 'json')
 			res.json(data);
 		else
