c23f3d3959e27040f81d73e363c327f4698d768d
Sebastien Nicoud <snicoud@expedia.com>
2016-12-10 21:16:32 -0500

    pass query params on

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var _ = require('lodash');
 var conf = require('../conf');
 var potos = require('../x/potos');
 var u = require('../x/utils');
+var qs = require('querystring');
 
 router.get('/pix', function(req, res, next) {
 
@@ -17,8 +18,14 @@ router.get('/pix', function(req, res, next) {
 	var init_zoom = u.validateInt(req.query.z, 10, 1000);
 	if (typeof init_zoom !== "undefined") public_conf.zoom = init_zoom;
 
+	// query string without path
+	var query_cp = _.clone(req.query);
+	delete query_cp.path;
+	var query_string = qs.stringify(query_cp);
+
 	potos.pix(fsdir, query_path)
 	.then(function (data) {
+		data.querystring = query_string;
 		data.conf = public_conf;
 		data.querypath = query_path;
 		data.path = path.normalize("/" + conf.pixpath + "/" + query_path);
